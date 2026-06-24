3206141dd6823e9887122e807ba8e8709f359920
Joao Dias <jd9198825@gmail.com>
2026-06-24 15:38:14 -0300

    Trabalhando com Cases, junto com CTE

--- Cases.sql
+++ Cases.sql
@@ -1,3 +1,6 @@
+
+--exemplo clássico de um case
+
 SELECT
     job_title_short,
     salary_hour_avg,
@@ -8,4 +11,31 @@ SELECT
     END AS salary_category
 FROM job_postings_fact
 WHERE salary_hour_avg IS NOT NULL
+LIMIT 10;
+
+--outro exemplo, usando CTE
+
+WITH salaries AS (
+SELECT
+    job_title_short,
+    salary_year_avg,
+    salary_hour_avg,
+    CASE
+        WHEN salary_year_avg IS NOT NULL THEN salary_year_avg
+        WHEN salary_hour_avg IS NOT NULL THEN salary_hour_avg*2080
+    END AS standardized_salary
+FROM job_postings_fact
+WHERE salary_year_avg IS NOT NULL OR salary_hour_avg IS NOT NULL
+)
+
+SELECT
+    *,
+    CASE
+        WHEN standardized_salary IS NULL THEN 'Missing'
+        WHEN standardized_salary < 75_000 THEN 'Low'
+        WHEN standardized_salary < 150_000 THEN 'Medium'
+        ELSE 'High'
+    END AS salary_bucket
+FROM salaries
+ORDER BY standardized_salary DESC
 LIMIT 10;
